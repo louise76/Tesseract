047a9777df35dde462d40729b65aa96dbbb36072
Apple <ytapplegamer@gmail.com>
2017-02-13 22:31:36 -0500

    New protocol + packet ids

---
+++ src/pocketmine/network/protocol/UpdateTradePacket.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace pocketmine\network\protocol;
+
+class UpdateTradePacket extends DataPacket{
+	
+	const NETWORK_ID = Info::UPDATE_TRADE_PACKET;
+	
+	//I'll finish this tomorrow, it's late
+	
+	public function decode(){
+		
+	}
+	
+	public function encode(){
+		
+	}
+}
